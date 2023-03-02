353112c7fe1843d4801271e8fdca4645d2077219
ipadhim <ipadhim@qq.com>
2023-03-02 17:32:30 +0800

    补全postcss

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
 /**
- * @author ipadhim
  * vite.config.js
  * SPCTeam配置
+ * @author ipadhim
  */
+
+import autoprefixer from "autoprefixer";
+import cssnanoPlugin from "cssnano";
 
 /** @type {import("vite").UserConfig} */
 export default {
-
-}
+    css: {
+        postcss: {
+            plugins: [
+                cssnanoPlugin(),
+                autoprefixer()
+            ]
+        }
+    }
+};
